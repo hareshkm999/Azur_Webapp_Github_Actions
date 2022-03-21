505a2bd68ddd58544770329bf59ad37b05829688
haresh musti <hareshkm999@gmail.com>
2022-03-21 17:32:37 +0530

    commit

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response
+import os
+from flask_cors import CORS, cross_origin
+import flask_monitoringdashboard as dashboard
+import json
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+dashboard.bind(app)
+CORS(app)
+
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return {"data": "Application ran successfully - FastAPI release v3.0 from app.py"}
+
+
+
+port = int(os.getenv("PORT", 5000))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5001
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
